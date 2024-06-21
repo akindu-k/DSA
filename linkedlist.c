d585a1035af18e55dcaee371da46f581307d7f93
akindu-k <akinduk619@gmail.com>
2024-06-21 14:09:24 +0530

    chore: Refactor linkedlist.c to use dynamic memory allocation

--- linkedlist.c
+++ linkedlist.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 typedef struct node
 {
-    int a;
+    int number;
     struct node *next;
 }node;
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
 
-    int a[3];
+    node *list = NULL;
     
+    for (int i=1; i < argc; i++)
+    {
+        int number = atoi(argv[i]);
+        
+        node *n = malloc(sizeof(node));
+        if (n==NULL)
+        {
+            return 1;
+        }
+        n->number = number;
+        n->next =list;
+
+        
+        list = n;
+    }
+
+    //print whole list
+    node *ptr = list;
+    while (ptr != NULL)
+    {
+        printf("%i\n",ptr->number);
+        
+    }
 }
